4feff0eace135cf68cd836d0b3b7325f7b0bc46f
UnitedPuggs <edpoulsonv@hotmail.com>
2024-09-05 16:20:55 -0700

    wip: manifest for PWA and some design experimentation

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,8 +1,7 @@
-<nav class="p-4 border-b border-black">
-    <div class="flex justify-center items-center lg:justify-start lg:items-start gap-4 text-xl">
-        <a href="/" class="hover:opacity-85 active:scale-90">home</a>
+<nav class="p-1 bg-lime-300">
+    <div class="flex flex-col justify-center items-center gap-2 text-lg">
+        <a href="/" class="hover:opacity-85 active:scale-90 w-20"><img src="logo.png" alt="logo"></a>
         <!-- add a logical branch to check for an account and THEN render animals-->
-        <a href="/animals" class="hover:opacity-85 active:scale-90">animals</a>
-        <a href="/" class="hover:opacity-85 active:scale-90">login</a>
+        <a href="/animals" class="hover:opacity-85 active:scale-90 border-2 rounded-lg border-black p-1 font-bold bg-white">Animals</a>
     </div>
 </nav>
