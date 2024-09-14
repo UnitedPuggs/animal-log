6b9cd57d6fce9805180bacbd4b02ea24a636680a
UnitedPuggs <edpoulsonv@hotmail.com>
2024-09-14 00:06:57 -0700

    fix: some dateselector styling

--- src/lib/Animals/DateSelector.svelte
+++ src/lib/Animals/DateSelector.svelte
@@ -3,14 +3,16 @@
     let checkedDays = [false, false, false, false, false, false, false];
 </script>
 
-<div class="border border-black rounded-lg p-2 w-fit">
+<div class="border border-black rounded-lg p-2 w-full">
     <div class="flex">
         {#each days as day}
-            <input type="checkbox" class="hidden" on:click={() => checkedDays[days.indexOf(day)] = !checkedDays[days.indexOf(day)]} id="check-{day}" bind:checked={checkedDays[days.indexOf(day)]}>
-            <label for="check-{day}" class="bg-sky-400 px-2 py-1 text-white {day == "Sun" ? "rounded-l-lg" : ""} {day == "Sat" ? "rounded-r-lg" : ""} {checkedDays[days.indexOf(day)] ? "bg-sky-900" : ""}"
-            >
-            {day}
-            </label>
+            <button type="button" class="w-full px-2 py-1 {day == "Sun" ? "rounded-l-lg" : "rounded-none"} {day == "Sat" ? "rounded-r-lg" : "rounded-none"} {checkedDays[days.indexOf(day)] ? "bg-sky-900" : "bg-sky-400"} text-white cursor-pointer" on:click={() => checkedDays[days.indexOf(day)] = !checkedDays[days.indexOf(day)]}>
+                <input type="checkbox" class="hidden" on:click={() => checkedDays[days.indexOf(day)] = !checkedDays[days.indexOf(day)]} id="check-{day}" bind:checked={checkedDays[days.indexOf(day)]}>
+                <label for="check-{day}" class="cursor-pointer"
+                >
+                {day}
+                </label>
+            </button>
         {/each}
     </div>
 </div>
