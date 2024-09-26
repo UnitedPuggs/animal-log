1ad1f7a504dbc0f8e9646d8c91ad2900488d9895
UnitedPuggs <edpoulsonv@hotmail.com>
2024-09-25 22:27:18 -0700

    something needs to be async

--- src/routes/animals/+layout.server.js
+++ src/routes/animals/+layout.server.js
@@ -1,6 +1,6 @@
 import { redirect } from "@sveltejs/kit";
 
-export function load({ cookies }) {
+export async function load({ cookies }) {
     if(!cookies.get('pb_auth')) { // not sure why locals seems to cause a race condition thing
         redirect(303, '/login')
     }
