a1ebbc9ec7cc4c8e1b6f4910cf69ca7c1a4e2662
UnitedPuggs <edpoulsonv@hotmail.com>
2024-09-25 22:26:22 -0700

    whoops

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,3 +1,7 @@
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
+
 export const load = async({ locals }) => {
     const user = locals.pb.authStore.model;
     
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,4 @@
 import { redirect } from '@sveltejs/kit'
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
-injectSpeedInsights();
 
 export function load({locals}) {
     if(!locals.pb.authStore.modal) {
