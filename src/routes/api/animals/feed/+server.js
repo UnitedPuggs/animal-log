b1682534c93fb0f203ed0c0cd7b0468bc98f430d
UnitedPuggs <edpoulsonv@hotmail.com>
2024-10-04 23:14:20 -0700

    feat: endpoint should work nfc stuff

--- src/routes/api/animals/feed/+server.js
+++ src/routes/api/animals/feed/+server.js
@@ -1,20 +1,22 @@
 import { pb } from '$lib/pocketbase';
 
-export async function GET({ request, locals }) {
+export async function GET({ url, locals }) {
     const user = locals.pb.authStore.model.id;
-    //const { animal, food } = await request.json();
-    //const record = await pb.collection('animals').getFirstListItem(`owner="${user}"`, {
-    //    filter: `name="${animal}"`
-    //});
+    const animal = url.searchParams.get("animal");
+    const record = await pb.collection('animals').getFirstListItem(`owner="${user}"`, {
+        filter: `name~"${animal}"`
+    });
+
     // All this shit just to get the animal id smh
-    //const animalId = record.id;
+    const animalId = record.id;
     
     const data = {
-        "food": "placeholder",
-        "animal": "n4goymvcbg31puy"
+        "food": "Food",
+        "animal": animalId
     };
 
     const feedRecord = await pb.collection("feedings").create(data);
-    
+    console.log(feedRecord);
+
     return new Response(null, { status: 204 });
 }
